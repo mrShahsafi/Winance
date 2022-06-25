0bbb216f4070d7910eb87f41859bb7c7b50acc2b
amir shahsafi <xvstanxv@gmail.com>
2022-06-25 13:00:56 +0430

    set site_header and title for admin panel

--- README.md
+++ README.md
@@ -1,4 +1,7 @@
 # Personal-Financial-Manager
 
 ![Wainance](https://github.com/mrShahsafi/Wainance/blob/main/static/wainance.png?raw=true)
-All of us need a personal application for the finance management 
+
+## All of us need a personal application for the finance management
+
+
--- winance/urls.py
+++ winance/urls.py
@@ -16,7 +16,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from graphene_django.views import GraphQLView
+from winance.settings import SITE_NAME
 
+admin.site.site_header = SITE_NAME
+admin.site.site_title = f"{SITE_NAME} Portal"
+admin.site.index_title = f"Welcome to {SITE_NAME} Portal"
 
 urlpatterns = [
     path("", admin.site.urls),
